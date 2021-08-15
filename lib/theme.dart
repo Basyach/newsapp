7c8207858c1034a790fa8b1935f0837e3cb02e6b
basyach <basyach@gmail.com>
2021-08-16 00:14:08 +0700

    first

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+Color blackcolor = Color(0xff000000);
+Color whitecolor = Color(0xffFFFFFF);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+
+  fontWeight: FontWeight.normal,
+  color: blackcolor
+);
